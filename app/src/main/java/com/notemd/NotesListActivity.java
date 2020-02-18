dcdea91840fd6142b8dccf5b7aeb6d567515a635
Gerry Fletcher <gerryfletch@users.noreply.github.com>
2020-02-18 16:10:51 +0000

    Basic Recyclable Note List (#9)

--- app/src/main/java/com/notemd/NotesListActivity.java
+++ app/src/main/java/com/notemd/NotesListActivity.java
@@ -1,37 +1,32 @@
 package com.notemd;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import android.view.View;
-
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class NotesListActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         this.renderNoteList();
     }
 
     private void renderNoteList() {
         RecyclerView recyclerView = findViewById(R.id.notesList);
+
         recyclerView.setHasFixedSize(true);
+
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        List<NoteMeta> notes = Arrays.asList(
-                new NoteMeta("Intro to Python, Class One", "TWO DAYS AGO", "YESTERDAY"),
-                new NoteMeta("", "", ""),
-                new NoteMeta("", "", "")
-        );
-
-        recyclerView.setAdapter(new NotesListAdapter(notes));
+        RecyclerView.Adapter adapter = new NotesListAdapter(new ArrayList<NoteMeta>());
+        recyclerView.setAdapter(adapter);
     }
 }
